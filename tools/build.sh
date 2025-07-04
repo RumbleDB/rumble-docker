47364f7e150e745d9f2e26ad71131be794d0fe3b
Ingo Müller <ingomueller@google.com>
2025-07-04 07:49:50 +0000

    Add metadata for Rumble v1.23.0.
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- tools/build.sh
+++ tools/build.sh
@@ -89,8 +89,9 @@ declare -A SPARK_VERSIONS=(
   [1.18.0]=3.2.4
   [1.19.0]=3.3.2
   [1.20.0]=3.3.2
-  [1.21.0]=3.4.0
-  [1.22.0]=3.5.0
+  [1.21.0]=3.4.4
+  [1.22.0]=3.5.6
+  [1.23.0]=3.5.6
 )
 
 if [[ -z "$spark_version" && -n "$rumble_version" ]]
@@ -205,6 +206,7 @@ declare -A RUMBLE_FILENAMES=(
   [1.21.0-3.4]=v1.21.0/rumbledb-1.21.0-for-spark-3.4.jar
   [1.22.0-3.4]=v1.22.0/rumbledb-1.22.0-for-spark-3.4-scala-2.12.jar
   [1.22.0-3.5]=v1.22.0/rumbledb-1.22.0-for-spark-3.5-scala-2.12.jar
+  [1.23.0-3.5]=v1.22.0/rumbledb-1.22.0-for-spark-3.5-scala-2.13.jar
 )
 
 if [[ -z "$rumble_url" && -n "$spark_version" ]]
